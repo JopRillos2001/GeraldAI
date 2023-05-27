76061f432e415d96fb033ed5ae843218b58c071f
BootBoost <npjkuijs@avans.nl>
2023-05-27 13:30:28 +0200

    Create PuzzleScene2

---
+++ Assets/Scripts/Scenes/PuzzleScene2Manager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleScene2Manager : MonoBehaviour
+{
+    private GameObject player;
+    private static PuzzleScene2Manager _instance;
+
+    public static PuzzleScene2Manager Instance {
+        get { return _instance; }
+    }
+
+    private void Awake() {
+        if (_instance != null && _instance != this) {
+            Destroy(this.gameObject);
+        } else {
+            _instance = this;
+        }
+    }
+
+    private void Start() {
+
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+}
