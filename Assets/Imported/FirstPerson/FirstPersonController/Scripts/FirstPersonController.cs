2cda40e40a34f7ca662798d8ca71a238018006bf
JopRillos2001 <47351860+JopRillos2001@users.noreply.github.com>
2023-05-29 23:37:36 +0200

    Level1 made

--- Assets/Imported/FirstPerson/FirstPersonController/Scripts/FirstPersonController.cs
+++ Assets/Imported/FirstPerson/FirstPersonController/Scripts/FirstPersonController.cs
@@ -178,6 +178,17 @@ namespace StarterAssets
 			// note: Vector2's == operator uses approximation so is not floating point error prone, and is cheaper than magnitude
 			// if there is no input, set the target speed to 0
 			if (_input.move == Vector2.zero) targetSpeed = 0.0f;
+			if (targetSpeed > 0.1f && Grounded) {
+
+				GetComponent<AudioSource>().enabled = true;
+				if (_input.sprint)
+					GetComponent<AudioSource>().pitch = 1.2f;
+				else
+					GetComponent<AudioSource>().pitch = 1f;
+			} else {
+				GetComponent<AudioSource>().enabled = false;
+			}
+
 
 			// a reference to the players current horizontal velocity
 			float currentHorizontalSpeed = new Vector3(_controller.velocity.x, 0.0f, _controller.velocity.z).magnitude;
