2c9b6c48aa5cdfc443298123c7fe76a548f7212c
JopRillos2001 <47351860+JopRillos2001@users.noreply.github.com>
2023-06-07 22:50:15 +0200

    NPC triggers removed and respawnitem added

---
+++ Assets/Scripts/Triggers/RespawnItem.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]  
+public class RespawnItem : MonoBehaviour
+{
+    private Vector3 spawnLocation;
+    private Quaternion spawnRotation;
+    private Rigidbody rb;
+    [SerializeField] private float minYLevel = -190;
+    private void Start() {
+        spawnLocation = transform.position;
+        spawnRotation = transform.rotation;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update() {
+        if (transform.position.y < minYLevel) {
+            rb.velocity = Vector3.zero;
+            transform.position = spawnLocation;
+            transform.rotation = spawnRotation;
+        }
+    }
+}
