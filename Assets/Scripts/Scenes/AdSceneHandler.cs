7119dc4142075c03e026bf46278995692e49773c
JopRillos2001 <47351860+JopRillos2001@users.noreply.github.com>
2023-06-12 14:40:59 +0200

    Skipbutton fix

--- Assets/Scripts/Scenes/AdSceneHandler.cs
+++ Assets/Scripts/Scenes/AdSceneHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityStandardAssets.Characters.ThirdPerson;
 
 public class AdSceneHandler : MonoBehaviour
@@ -11,19 +12,21 @@ public class AdSceneHandler : MonoBehaviour
     [SerializeField] private float bcDelay = 25f;
     [SerializeField] private SceneEnum nextScene;
     [SerializeField] private float sceneDelay = 48f;
+    [SerializeField] private float mouseLockDelay = 46f;
     [SerializeField] private List<VirCamStamp> virCamsStamps;
+    [SerializeField] private Button skipButton;
 
     private void Start() {
         Invoke("DimLight", delay);
         Invoke("EnAI", bcDelay);
         Invoke("NextScene", sceneDelay);
 
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
         foreach (VirCamStamp vcs in virCamsStamps) {
             StartCoroutine(SwitchCam(vcs.virCam, vcs.delay));
         }
-
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
     }
 
     private IEnumerator SwitchCam(GameObject virCam, float delay) {
@@ -41,6 +44,7 @@ public class AdSceneHandler : MonoBehaviour
     }
 
     private void NextScene() {
+        MouseLock();
         GameManager.Instance.GetComponent<SceneHandler>().SceneLoad(nextScene);
     }
 
@@ -48,5 +52,14 @@ public class AdSceneHandler : MonoBehaviour
         foreach (VirCamStamp vcs in virCamsStamps) {
             vcs.virCam.SetActive(false);
         }
+    }
+
+    private void MouseLock() {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void disableButton() {
+        skipButton.interactable = false;
     }
 }
